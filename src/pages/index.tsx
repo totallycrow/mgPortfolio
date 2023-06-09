3e4c3365df7a48eb2b85a315a9e020c5883e4219
Adrian Gajda <adrian.gajda@hotmail.com>
2023-06-09 14:50:32 +0100

    initialise header component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,16 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import { Header } from "@/components/header/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main>
-      <div>Hello World!</div>
-    </main>
+    <div>
+      <Header />
+      <main>
+        <div>Hello World!</div>
+      </main>
+    </div>
   );
 }
