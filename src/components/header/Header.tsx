ad7a5f0964e1aee155fe4862ae9ce9764639f358
Adrian Gajda <adrian.gajda@hotmail.com>
2023-06-09 16:42:53 +0100

    center elements

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -5,20 +5,21 @@ import { EmailIcon } from "../utils/icons/EmailIcon";
 
 export const Header = () => {
   return (
-    <header>
-      <nav>
-        <ul>
+    <header className="w-screen flex content-center justify-center bg-slate-200">
+      <nav className="flex w-1/3 justify-center">
+        <ul className="w-full flex justify-center">
           <li>Work</li>
           <li>Misc</li>
           <li>About</li>
           <li>Contact</li>
         </ul>
       </nav>
-      <div>Logo</div>
-
-      <InstagramIcon />
-      <LinkedinIcon />
-      <EmailIcon />
+      <div className="flex w-1/3 justify-center">Logo</div>
+      <div className="flex w-1/3 justify-center">
+        <InstagramIcon />
+        <LinkedinIcon />
+        <EmailIcon />
+      </div>
     </header>
   );
 };
