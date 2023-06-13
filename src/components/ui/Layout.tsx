7cbb29c049f6c433a8eada662c737bbca2abd6ae
Adrian Gajda <adrian.gajda@hotmail.com>
2023-06-13 13:19:20 +0100

    setup Layout

---
+++ src/components/ui/Layout.tsx
@@ -0,0 +1,12 @@
+import { Header } from "../header/Header";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="">
+      <Header />
+      <div className="container mx-auto">
+        <main>{children}</main>
+      </div>
+    </div>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,47 +1,42 @@
 import Image from "next/image";
-import { Inter } from "next/font/google";
-import { Header } from "@/components/header/Header";
-import { GridContainer } from "@/components/GridContainer";
-import { GridItemCard } from "@/components/GridItemCard";
-import StaticImageCard from "@/components/StaticImageCard";
-import ShowcaseImageCard from "@/components/ShowcaseImageCard";
 
-const inter = Inter({ subsets: ["latin"] });
+import { Header } from "@/components/header/Header";
+import { GridContainer } from "@/components/grid/GridContainer";
+
+import StaticImageCard from "@/components/grid/StaticImageCard";
+import ShowcaseImageCard from "@/components/grid/ShowcaseImageCard";
 
 export default function Home() {
   return (
     <div>
-      <Header />
-      <div className="container mx-auto">
-        <main>
-          <div>Hello World!</div>
-          <GridContainer>
-            <ShowcaseImageCard
-              imageUrl="https://via.placeholder.com/1080x1080/eee?text=1:1"
-              hoverText="Lorem ipsum dolor sit amet"
-              link="#"
-            />
-            <ShowcaseImageCard
-              imageUrl="https://via.placeholder.com/1080x1080/eee?text=1:1"
-              hoverText="Lorem ipsum dolor sit amet"
-              link="#"
-            />
-            <ShowcaseImageCard
-              imageUrl="https://via.placeholder.com/1080x1080/eee?text=1:1"
-              hoverText="Lorem ipsum dolor sit amet"
-              link="#"
-            />
-            <ShowcaseImageCard
-              imageUrl="https://via.placeholder.com/1080x1080/eee?text=1:1"
-              hoverText="Lorem ipsum dolor sit amet"
-              link="#"
-            />
+      <main>
+        <div>Hello World!</div>
+        <GridContainer>
+          <ShowcaseImageCard
+            imageUrl="https://via.placeholder.com/1080x1080/eee?text=1:1"
+            hoverText="Lorem ipsum dolor sit amet"
+            link="#"
+          />
+          <ShowcaseImageCard
+            imageUrl="https://via.placeholder.com/1080x1080/eee?text=1:1"
+            hoverText="Lorem ipsum dolor sit amet"
+            link="#"
+          />
+          <ShowcaseImageCard
+            imageUrl="https://via.placeholder.com/1080x1080/eee?text=1:1"
+            hoverText="Lorem ipsum dolor sit amet"
+            link="#"
+          />
+          <ShowcaseImageCard
+            imageUrl="https://via.placeholder.com/1080x1080/eee?text=1:1"
+            hoverText="Lorem ipsum dolor sit amet"
+            link="#"
+          />
 
-            <StaticImageCard imageUrl="https://via.placeholder.com/1080x1080/eee?text=1:1" />
-            <StaticImageCard imageUrl="https://via.placeholder.com/1080x1080/eee?text=1:1" />
-          </GridContainer>
-        </main>
-      </div>
+          <StaticImageCard imageUrl="https://via.placeholder.com/1080x1080/eee?text=1:1" />
+          <StaticImageCard imageUrl="https://via.placeholder.com/1080x1080/eee?text=1:1" />
+        </GridContainer>
+      </main>
     </div>
   );
 }
