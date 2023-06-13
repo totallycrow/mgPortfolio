05c00c0b5b86b2fa1bb4a959d1a7e1267ed74c95
Adrian Gajda <adrian.gajda@hotmail.com>
2023-06-13 12:20:22 +0100

    create grid boilerplate

---
+++ src/components/GridContainer.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const GridContainer = ({ children }: { children: React.ReactNode }) => {
+  return <div>GridContainer
+    <div>
+        {children}
+    </div>
+
+  </div>;
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import { Header } from "@/components/header/Header";
+import { GridContainer } from "@/components/GridContainer";
+import { GridItemCard } from "@/components/GridItemCard";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,6 +13,11 @@ export default function Home() {
       <div className="container mx-auto">
         <main>
           <div>Hello World!</div>
+          <GridContainer>
+            <GridItemCard />
+            <GridItemCard />
+            <GridItemCard />
+          </GridContainer>
         </main>
       </div>
     </div>
