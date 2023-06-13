6812a5c52a7ccdaed4a9b7495c0811dfe9205239
Adrian Gajda <adrian.gajda@hotmail.com>
2023-06-13 15:09:58 +0100

    uncomment imagecard

--- src/components/grid/ShowcaseImageCard.tsx
+++ src/components/grid/ShowcaseImageCard.tsx
@@ -9,19 +9,18 @@ const ImageCard = ({
   link: string;
 }) => {
   return (
-    <div>test</div>
-    // <div className="relative w-full h-full overflow-hidden rounded bg-slate-200">
-    //   <a href="#">
-    //     <img
-    //       className="w-full h-full transition-opacity duration-500 ease-in-out"
-    //       src={imageUrl}
-    //       alt="card content"
-    //     />
-    //     <div className="absolute inset-0 bg-white opacity-0 hover:opacity-100 flex items-center justify-center transition-opacity duration-500 ease-in-out">
-    //       <span className="text-black text-center">{hoverText}</span>
-    //     </div>
-    //   </a>
-    // </div>
+    <div className="relative w-full h-full overflow-hidden rounded bg-slate-200">
+      <a href="#">
+        <img
+          className="w-full h-full transition-opacity duration-500 ease-in-out"
+          src={imageUrl}
+          alt="card content"
+        />
+        <div className="absolute inset-0 bg-white opacity-0 hover:opacity-100 flex items-center justify-center transition-opacity duration-500 ease-in-out">
+          <span className="text-black text-center">{hoverText}</span>
+        </div>
+      </a>
+    </div>
   );
 };
 
