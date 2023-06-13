343b6b6ff1569cdbb03148353992b4908f59896a
Adrian Gajda <adrian.gajda@hotmail.com>
2023-06-13 14:33:04 +0100

    boilerplate navigation between work pages with bottom arrows

--- src/components/grid/GridContainer.tsx
+++ src/components/grid/GridContainer.tsx
@@ -3,7 +3,6 @@ import React from "react";
 export const GridContainer = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="w-full mx-auto text-center">
-      GridContainer
       <div className="grid grid-cols-2 gap-4 content-center justify-center w-full mx-auto text-center">
         {children}
       </div>
